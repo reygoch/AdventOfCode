4a2db85a7bff52d028a791afd5c38df791405d4f
Luka Hadžiegrić <reygoch@gmail.com>
2018-12-01 13:32:16 +0100

    Solution for Day 01

---
+++ 2018/Day01.hs
@@ -0,0 +1,29 @@
+import Data.Set ( member, insert )
+import Data.List.Split ( splitOn )
+--
+
+main :: IO ()
+main = do
+  input <- ( parser . lines ) <$> readFile "Day01.input"
+
+  putStr "Part1: "
+  print $ part1 input
+
+  putStr "Part2: "
+  print $ part2 input
+
+--
+
+parser :: [ String ] -> [ Int ]
+parser = map $ \x -> ( if head x == '-' then negate else id ) $ read $ tail x
+
+--
+
+part1 :: [ Int ] -> Int
+part1 = sum
+
+--
+
+part2 :: [ Int ] -> Int
+part2 = go mempty . scanl (+) 0 . cycle
+  where go ps ( x:xs ) = if x `member` ps then x else go ( x `insert` ps ) xs
